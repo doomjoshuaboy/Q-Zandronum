992aa095277ed64157f30ec544142b9919670b20
Joshua Surace <doomjoshuaboy@gmail.com>
2022-02-16 18:47:58 +1100

    Fixed a missed error.
    Again
    
    derp
    
    x2
    
    Fixed compiling errors
    
    Fixed more errors again
    
    Fixed more compiling errors
    
    Fixed more errors again
    
    Fixed compiling errors
    
    x2
    
    derp
    
    Again
    
    Fixed a missed error.

--- src/sv_main.cpp
+++ src/sv_main.cpp
@@ -296,7 +296,8 @@ CVAR( String, tspg_branding_url, "https://allfearthesentinel.net", CVAR_SERVERIN
 CUSTOM_CVAR( String, sv_adminlistfile, "adminlist.txt", CVAR_ARCHIVE|CVAR_NOSETBYACS|CVAR_NOSET )
 #else
 CUSTOM_CVAR( String, sv_adminlistfile, "adminlist.txt", CVAR_ARCHIVE|CVAR_NOSETBYACS)
-#endif{
+#endif
+{
 	if ( NETWORK_GetState( ) != NETSTATE_SERVER )
 		return;
 
@@ -6907,7 +6908,7 @@ FString CLIENT_s::GetAccountName() const
 		return result;
 	}
 }
-`
+
 //*****************************************************************************
 // [K6]
 extern std::vector<MAPROTATIONENTRY_t>	g_MapRotationEntries;
